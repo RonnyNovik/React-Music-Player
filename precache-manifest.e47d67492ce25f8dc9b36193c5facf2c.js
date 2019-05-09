bbad1654814a8fab313b109a36bbb3e5fa059587
RonnyNovik <novikdesigndev@gmail.com>
2019-05-09 20:09:06 +0300

    Updates

---
+++ precache-manifest.e47d67492ce25f8dc9b36193c5facf2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f84e4538ad87989f9e16",
+    "url": "/React-Music-Player/static/css/main.95645381.chunk.css"
+  },
+  {
+    "revision": "f84e4538ad87989f9e16",
+    "url": "/React-Music-Player/static/js/main.b750fb5a.chunk.js"
+  },
+  {
+    "revision": "645d9438897152eac65d",
+    "url": "/React-Music-Player/static/js/runtime~main.abf95913.js"
+  },
+  {
+    "revision": "63e4d9fbaf187e8fa20a",
+    "url": "/React-Music-Player/static/js/2.0ca261a4.chunk.js"
+  },
+  {
+    "revision": "d2fc5fa13d644e4f7073a8b6dee742c7",
+    "url": "/React-Music-Player/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/RonnyNovik/React-Music-Player/precache-manifest.bf09041067b85db7e2e3693a37b093c5.js"
+  "/React-Music-Player/precache-manifest.e47d67492ce25f8dc9b36193c5facf2c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/RonnyNovik/React-Music-Player/index.html", {
+workbox.routing.registerNavigationRoute("/React-Music-Player/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
